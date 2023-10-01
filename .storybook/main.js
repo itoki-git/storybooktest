9b73f9907904926be54c1122c208744ba6bd5c00
itoki-git <zxxgxdfggd@gmail.com>
2023-10-01 16:31:49 +0900

    commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,19 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+   '@storybook/addon-a11y',
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  docs: {
-    autodocs: "tag",
+  features: {
+    interactionsDebugger: true,
   },
 };
-export default config;
